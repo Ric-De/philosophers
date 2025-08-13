bfadb64784045b7992342291072b2c4a4ed54951
Ric-De <ricdellazari@gmail.com>
2025-08-13 16:40:24 +0200

    Differences between: pthread_cond_signal and pthread_cond_broadcast

---
+++ research/codevault/threads_main10.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* Basically this order of execution ocurred:
+ *
+ * car1: lock -> cond_wait -> unlock
+ * car2: lock -> cond_wait -> unlock
+ * fueler1: lock
+ * car3: lock(waiting on lock)
+ * car4: lock(waiting on lock)
+ *
+ * */
+
+int	fuel = 0;
+pthread_mutex_t	mutexFuel;
+pthread_cond_t	condFuel;
+
+void	*fuel_filling(void *arg)
+{
+	for (int i = 0; i < 5; i++)
+	{
+		pthread_mutex_lock(&mutexFuel);
+		fuel += 30;
+		printf("Filled fuel... %d\n", fuel);
+		pthread_mutex_unlock(&mutexFuel);
+		pthread_cond_broadcast(&condFuel);
+		sleep(1);
+	}
+}
+
+void	*car(void *arg)
+{
+	pthread_mutex_lock(&mutexFuel);
+	while (fuel < 40)
+	{
+		printf("No fuel. Waiting...\n");
+		pthread_cond_wait(&condFuel, &mutexFuel);
+		// Equivalent to:
+		// pthread_mutex_unlock(&mutexFuel);
+		// wait for signal on condFuell;
+		// pthread_mutex_lock(&mutexFuel);
+	}
+	fuel -= 40;
+	printf("Got fuel. Now left: %d\n", fuel);
+	pthread_mutex_unlock(&mutexFuel);
+
+	// printf("Here to get fuel\n");
+}
+
+int	main()
+{
+	pthread_t	th[6];
+	pthread_mutex_init(&mutexFuel, NULL);
+	pthread_cond_init(&condFuel, NULL);
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (i == 4 || i == 5)
+		{
+
+			if (pthread_create(&th[i], NULL, &fuel_filling, NULL) != 0)
+			{
+				perror("Failed to create thread");
+			}
+		}
+		else
+		{
+			if (pthread_create(&th[i], NULL, &car, NULL) != 0)
+			{
+				perror("Failed to create thread");
+			}
+		}
+	}
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (pthread_join(th[i], NULL) != 0)
+		{
+			perror("Failed to join thread");
+		}
+	}
+	pthread_mutex_destroy(&mutexFuel);
+	pthread_cond_destroy(&condFuel);
+	return (0);
+}
