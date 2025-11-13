27e0c2048758470684128ff2f086cfa7a7a93f18
Ric-De <piaric42@protonmail.com>
2025-11-13 15:15:09 +0100

    philo plan and project initiated again. Let's rollgit add .!

---
+++ philo/main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdellaza <rdellaza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:55:01 by rdellaza          #+#    #+#             */
+/*   Updated: 2025/11/13 14:59:02 by rdellaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+/*  TODO: put more functions here on the go... */
+
+int	main(int argc, char **argv)
+{
+	printf("DEBUG: Here motherfucker Philosophers, new attempt!\n");
+	printf("DEBUG: argc = %d\n", argc);
+	if (argc >= 2)
+		printf("DEBUG: First argument = %s\n", argv[1]);
+	/* TODO: Parse arguments */
+	/* TODO: Initialize data structures */
+	/* TODO: Create threads */
+	/* TODO: Wait for threads */
+	/* TODO: Cleanup */
+	return (0);
+}
---
+++ philo/philo.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rdellaza <rdellaza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:46:20 by rdellaza          #+#    #+#             */
+/*   Updated: 2025/11/13 14:54:53 by rdellaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <sys/time.h>
+
+/* TODO: More structs and the whole prototypes as I build the thing */
+
+/* =============== STRUCTURES =============== */
+
+typedef struct	s_data
+{
+	int	nb_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	must_eat_count;
+	long	start_time;
+	int	someone_died;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	death_mutex;
+	pthread_mutex_t	*forks;
+	struct s_philo	*philos;
+}	t_data;
+
+typedef struct	s_philo
+{
+	int	id;
+	int	meals_eaten;
+	long	last_meal_time;
+	pthread_t	thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_data	*data;
+}	t_philo;
+
+/* =============== FUNCTION PROTOTYPES =============== */
+
+/* TODO: For function prototypes I create them */
+
+#endif
